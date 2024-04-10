7834b0fc02de7bde5bf778bc9c81dc66441e30f9
Alexandre Angrignon <alex040892@gmail.com>
2024-04-10 18:20:16 +1200

    Login Page Frontend

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -1,22 +1,11 @@
 import { Link } from "react-router-dom";
 
-const Register = () => {
+const Login = () => {
   return (
     <div className="d-flex justify-content-center align-items-center bg-primary vh-100">
       <div className="bg-white p-3 rounded w-25">
-        <h2>Sign Up</h2>
+        <h2>Sign In</h2>
         <form>
-          <div className="mb-3">
-            <label htmlFor="name">
-              <strong>Name</strong>
-            </label>
-            <input
-              type="text"
-              placeholder="Enter Name"
-              name="name"
-              className="form-control rounded-0"
-            />
-          </div>
           <div className="mb-3">
             <label htmlFor="email">
               <strong>Email</strong>
@@ -39,13 +28,14 @@ const Register = () => {
               className="form-control rounded-0"
             />
           </div>
-          <button className="btn btn-success w-100 rounded-0">Sign Up</button>
+
+          <button className="btn btn-success w-100 rounded-0">Log In</button>
           <p>You are agree to our terms and policies</p>
           <Link
-            to="/login"
+            to="/register"
             className="btn btn-default border w-100 bg-light rounded-0 text-decoration-none"
           >
-            Login
+            Create Account
           </Link>
         </form>
       </div>
@@ -53,4 +43,4 @@ const Register = () => {
   );
 };
 
-export default Register;
+export default Login;
