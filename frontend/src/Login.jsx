0a760a65c9298eaa7648b95775077b8b85663ef5
Alexandre Angrignon <alex040892@gmail.com>
2024-04-13 12:48:24 +1200

    login page API call

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -1,11 +1,33 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Login = () => {
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+  });
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios
+      .post("http://localhost:8081/login", values)
+      .then((res) => {
+        if (res.data.Status === "Successful Registration") {
+          navigate("/");
+        } else {
+          alert("Error");
+        }
+      })
+      .then((err) => console.log(err));
+  };
   return (
     <div className="d-flex justify-content-center align-items-center bg-primary vh-100">
       <div className="bg-white p-3 rounded w-25">
         <h2>Sign In</h2>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="mb-3">
             <label htmlFor="email">
               <strong>Email</strong>
@@ -15,6 +37,7 @@ const Login = () => {
               placeholder="Enter Email"
               name="email"
               className="form-control rounded-0"
+              onChange={(e) => setValues({ ...values, email: e.target.value })}
             />
           </div>
           <div className="mb-3">
@@ -26,6 +49,9 @@ const Login = () => {
               placeholder="Enter Password"
               name="password"
               className="form-control rounded-0"
+              onChange={(e) =>
+                setValues({ ...values, password: e.target.value })
+              }
             />
           </div>
 
